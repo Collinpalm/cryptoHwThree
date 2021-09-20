c2deba61bbbdc3f9e4142a808aecf86e1b125571
Collin Palm <collinpalm@gmail.com>
2021-09-20 17:14:48 -0400

    working on brute

--- hillBruteForce.py
+++ hillBruteForce.py
@@ -9,6 +9,21 @@ run this on the command line with the text to be decrypted
 
 def decrypt():
     ciphertext = sys.argv[1]
+    plain = ""
+    temp = [0, 0]
+    for i in range(0, 25):
+        for j in range(0, 25):
+            for k in range(0, 25):
+                for l in range(0, 25):
+                    key = [i, j, k, l]
+                    for el in range(0, ciphertext.len):
+                        if(el%2 == 0):
+                            temp[0] = ord(ciphertext[el])-97
+                            temp[1] = ord(ciphertext[el+1])-97
+                            plain += chr(((key[0]*temp[0] + key[1]*temp[1])%26)+97)
+                            plain += chr(((key[2]*temp[0] + key[3]*temp[1])%26)+97)
+
+
     
         
     
