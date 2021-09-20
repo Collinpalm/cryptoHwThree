885a594bf798f5164cdae12264a567f984f8bd55
Collin Palm <collinpalm@gmail.com>
2021-09-20 16:55:03 -0400

    added brute force script

---
+++ hillBruteForce.py
@@ -0,0 +1,24 @@
+import sys
+import re 
+
+"""
+run this on the command line with the text to be decrypted
+"""
+    
+
+
+def decrypt():
+    ciphertext = sys.argv[1]
+    
+        
+    
+    
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    decrypt()
